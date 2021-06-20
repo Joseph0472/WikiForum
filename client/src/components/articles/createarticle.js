e8367d2f7a21d5a804ea8c27dc98763d8a54c509
Ajon <rushjp@yeah.net>
2021-06-21 02:01:13 +1200

    Optimize the fetching experience

--- client/src/components/articles/createarticle.js
+++ client/src/components/articles/createarticle.js
@@ -2,8 +2,8 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {createArticle} from '../../redux/actions/articleactions'
 import FetchArticle from '../fetcharticle'
-import {TokenAnnotator, TextAnnotator} from 'react-text-annotate'
-import SelectionHighlighter from "react-highlight-selection";
+// import {TokenAnnotator, TextAnnotator} from 'react-text-annotate'
+// import SelectionHighlighter from "react-highlight-selection";
 
 
 class CreateArticle extends React.Component {
@@ -15,34 +15,36 @@ class CreateArticle extends React.Component {
         htmlContent:''
     }
     
-    constructor() {
-        super();
-        this.selectionHandler = this.selectionHandler.bind(this);
-      }
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
     handleChange = (e) => {
-        console.log(e)
         this.setState({
             [e.target.id] : e.target.value
         })
     }
     
     handleSubmit = (e) => {
-        e.preventDefault();
+        // e.preventDefault();
         //console.log('the article is(from createarti com): ',this.state)
-        this.props.createArticle(this.state)
-    }
-
-    selectionHandler(selection) {
-        //do something with selection
-        console.log(selection);
+        //this.props.createArticle(this.state)
+        if(e.nativeEvent.submitter.name === "createArticle") {
+            alert('A name was submitted: ' + this.state.value);
+      } else {
+        console.log('another btn clicked.')
       }
+      e.preventDefault();
+    }
 
     render() {
         return(
             <div className="container">
                 <form onSubmit={this.handleSubmit} className="white" id="createarticleform">
                 <h4>New Article</h4>
+                <FetchArticle />
                 <div className="input-field">
                     <label htmlFor="title">Title</label>
                     <input type="text" id="title" onChange={this.handleChange} required/>
@@ -52,9 +54,8 @@ class CreateArticle extends React.Component {
                     <textarea id="content" className="materialize-textarea" style={{height: 500}} onChange={this.handleChange} required></textarea>
                 </div>
                 <div className="input-field">
-                    <button className="btn pink lighten-1 z-depth-0">Create</button>
+                    <button className="btn pink lighten-1 z-depth-0" name = 'createArticle' type="submit">Create</button>
                 </div>
-                <FetchArticle />
                 {console.log(this.state.content)}
                 </form>
             </div>
--- client/src/components/fetcharticle.js
+++ client/src/components/fetcharticle.js
@@ -1,20 +1,16 @@
-import React ,{Component} from 'react'
+import React , {Component} from 'react'
 import $ from 'jquery'
 
 export default class FetchArticle extends Component{
     constructor(props){
-        super()
+        super(props)
         this.state = {}
     }
     
-    // Consider: use hook method to execute ajax request to fetch full article content
-    // Or use async fetch in serverside
-
+    //TODO: Optimize the fetching process. Ideally, users can choose specific graph and the content. users should not edit the content.
     render(){
         function fetchArticle(){
             var searchTerm = document.getElementById('inputSearchTerm').value
-            // console.log(searchTerm)
-            
         $.ajax({
             url: "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&titles="+ searchTerm +"&redirects=true",
             headers: {
@@ -25,10 +21,7 @@ export default class FetchArticle extends Component{
             dataType: 'jsonp',
             data: '',
             success: function(data) {
-                // this.setState({data: data});   // notice
-                // console.log(data.query.pages)
                 var dataNum = Object.keys(data.query.pages)[0]
-                console.log(data.query.pages)
                 if(dataNum == -1){
                     $('#articleData').empty()
                     $('#articleData').append(`<h1>Sorry, no such article titiled by: ${data.query.pages[dataNum].title}, please try to search in Wikipedia and paste the article titile here.</h1>`)
@@ -47,7 +40,7 @@ export default class FetchArticle extends Component{
         return(
             <div>
                 <input className="input-field" type="text" placeholder="Enter a search term" id="inputSearchTerm"></input>
-                <button className="btn pink lighten-1 z-depth-0" type="submit" id="fetch" onClick={()=>fetchArticle()}>Fectch </button>
+                <button className="btn pink lighten-1 z-depth-0" id="fetch" onClick = {() => fetchArticle()}>Fectch from Wikipedia</button>
                 <div id="articleData"></div>
             </div>
         )
