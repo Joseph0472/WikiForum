c6cdc2395ebb5ec28f5100540d15009d93c1cb0a
Ajon <rushjp@yeah.net>
2021-06-22 23:57:53 +1200

    slightly improve of the api fetching

--- client/src/components/fetcharticle.js
+++ client/src/components/fetcharticle.js
@@ -11,35 +11,38 @@ export default class FetchArticle extends Component{
     render(){
         function fetchArticle(){
             var searchTerm = document.getElementById('inputSearchTerm').value
-        $.ajax({
-            url: "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&titles="+ searchTerm +"&redirects=true",
-            headers: {
-                'Access-Control-Allow-Origin' : '*',
-                'Content-Type' : 'application/json'
-            },
-            method: 'GET',
-            dataType: 'jsonp',
-            data: '',
-            success: function(data) {
-                var dataNum = Object.keys(data.query.pages)[0]
-                if(dataNum == -1){
-                    $('#articleData').empty()
-                    $('#articleData').append(`<h1>Sorry, no such article titiled by: ${data.query.pages[dataNum].title}, please try to search in Wikipedia and paste the article titile here.</h1>`)
-                }
-                else {
-                    $('#articleData').empty()
-                    $('#articleData').append(`<h1>${data.query.pages[dataNum].title}</h1>`)
-                    $('#articleData').append(data.query.pages[dataNum].extract)
-                }
-            },
-            error: function(xhr, status, err) {
-                console.error('article not found');
+            if(searchTerm) {
+                $.ajax({
+                    url: "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&titles="+ searchTerm +"&redirects=true",
+                    headers: {
+                        'Access-Control-Allow-Origin' : '*',
+                        'Content-Type' : 'application/json'
+                    },
+                    method: 'GET',
+                    dataType: 'jsonp',
+                    data: '',
+                    success: function(data) {
+                        console.log(data)
+                        var dataNum = Object.keys(data.query.pages)[0]
+                        if(dataNum == -1){
+                            alert("Wikipedia page not found, please confirm the key word. Please try to copy and paste the original title.");
+                        }
+                        else {
+                            $('#articleData').empty()
+                            // $('#articleData').append(data.query.pages[dataNum].title)
+                            $('#articleData').append(data.query.pages[dataNum].extract)
+                        }
+                    },
+                    error: function(xhr, status, err) {
+                        console.error('article not found');
+                    }
+                });
             }
-        });
+
     }
         return(
             <div>
-                <input className="input-field" type="text" placeholder="Enter a search term" id="inputSearchTerm"></input>
+                <input className="input-field" type="text" placeholder="Enter a search term" id="inputSearchTerm" required></input>
                 <button className="btn pink lighten-1 z-depth-0" id="fetch" onClick = {() => fetchArticle()}>Fectch from Wikipedia</button>
                 <div id="articleData"></div>
             </div>
